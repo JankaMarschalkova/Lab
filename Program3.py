fcefcfc9073787f23f528a0bfd095942c220ea09
JankaMarschalkova <76066467+JankaMarschalkova@users.noreply.github.com>
2022-09-15 02:36:48 +0200

    Add files via upload

--- Program3.py
+++ Program3.py
@@ -1,4 +1,6 @@
 print("Welcome to the Flarsheim Guesser!")
+
+# guessing start and while loop will end when the player input n
 answer = 'y'
 while answer != 'n':
     if answer == 'y':
@@ -8,6 +10,7 @@ while answer != 'n':
         divided_five = 0
         divided_seven = 0
 
+        # while loop will continue asking a question about remainder divided by 3 until there is valid input
         while True:
             divided_three = int(input("What is the remainder when your number is divided by 3 ?"))
             if divided_three < 0:
@@ -17,6 +20,8 @@ while answer != 'n':
             else:
                 break
         print()
+
+        # while loop will continue asking a question about remainder divided by 5 until there is valid input
         while True:
             divided_five = int(input("What is the remainder when your number is divided by 5 ?"))
             if divided_five < 0:
@@ -26,6 +31,8 @@ while answer != 'n':
             else:
                 break
         print()
+
+        # while loop will continue asking a question about remainder divided by 7 until there is valid input
         while True:
             divided_seven = int(input("What is the remainder when your number is divided by 7 ?"))
             if divided_seven < 0:
@@ -35,6 +42,7 @@ while answer != 'n':
             else:
                 break
 
+        # for loop from 1 to 100 will check number by number if the remainders are the same as the player input before
         for number in range(1, 101):
             if number % 3 == divided_three and number % 5 == divided_five and number % 7 == divided_seven:
                 result = number
